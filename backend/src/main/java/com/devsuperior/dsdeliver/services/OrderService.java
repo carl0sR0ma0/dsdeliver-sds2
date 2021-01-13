4a0545b72b1b5fd4fdeec1ba7bc9b5d93876b42c
carl0sR0ma0 <cvpromao@gmail.com>
2021-01-12 23:49:24 -0300

    PUT orders/{id}/delivered

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -26,19 +26,27 @@ public class OrderService {
 	private ProductRepository productRepository;
 
 	@Transactional(readOnly = true)
-	public List<OrderDTO> findAll(){
+	public List<OrderDTO> findAll() {
 		List<Order> list = repository.findOrderWithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 	}
 	
 	@Transactional
-	public OrderDTO insert(OrderDTO dto){
+	public OrderDTO insert(OrderDTO dto) {
 		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),
 				Instant.now(), OrderStatus.PENDING);
 		for (ProductDTO p : dto.getProducts()) {
 			Product product = productRepository.getOne(p.getId());
 			order.getProducts().add(product);
 		}
+		order = repository.save(order);
+		return new OrderDTO(order);
+	}
+	
+	@Transactional
+	public OrderDTO setDelivered(Long id){
+		Order order = repository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
 		order = repository.save(order);
 		return new OrderDTO(order);
 	}
