d1b5d6038d3373bc639a3115c31a602d8caac8f8
carl0sR0ma0 <cvpromao@gmail.com>
2021-01-14 23:55:09 -0300

    Add orders list

--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ScrollView, View } from 'react-native';
 import Header from '../Header';
+import OrderCard from "../OrderCard";
 
 function Orders() {
   return (
     <>
       <Header />
-      <View>
-        <Text>Listagem de Pedidos</Text>
-      </View>
+      <ScrollView style={styles.container}>
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+      </ScrollView>
     </>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    paddingRight: '5%',
+    paddingLeft: '5%'
+  }
+});
 
 export default Orders;
