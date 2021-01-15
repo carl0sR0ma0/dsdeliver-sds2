cba6f916479915bc0f50c5d1882b9d52b9b822c0
carl0sR0ma0 <cvpromao@gmail.com>
2021-01-15 01:39:25 -0300

    Add link of  Google Maps for route of delivery

--- front-mobile/src/OrderDetails/index.tsx
+++ front-mobile/src/OrderDetails/index.tsx
@@ -1,6 +1,6 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { StyleSheet, View, Text, Alert } from 'react-native';
+import { StyleSheet, View, Text, Alert, Linking } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import { confirmDelivery } from '../api';
 import Header from '../Header';
@@ -34,12 +34,16 @@ function OrderDetails({ route }: Props) {
       })
   }
 
+  const handleStartNavigation = () => {
+    Linking.openURL(`https://www.google.com/maps/dir/?api=1&travelmode=driving&dir_action=navigate&destination=${order.latitude},${order.longitude}`);
+  }
+
   return (
     <>
       <Header />
       <View style={styles.container}>
         <OrderCard order={order} />
-        <RectButton style={styles.button}>
+        <RectButton style={styles.button} onPress={handleStartNavigation}>
           <Text style={styles.buttonText}>INICIAR NAVEGAÇÃO</Text>
         </RectButton>
         <RectButton style={styles.button} onPress={handleConfirmDelivery}>
