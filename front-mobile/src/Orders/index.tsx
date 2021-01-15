056392285f83a14d7a21d406b336da5462da9302
carl0sR0ma0 <cvpromao@gmail.com>
2021-01-14 23:41:20 -0300

    Add Routes in app

---
+++ front-mobile/src/Orders/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from '../Header';
+
+function Orders() {
+  return (
+    <>
+      <Header />
+      <View>
+        <Text>Listagem de Pedidos</Text>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default Orders;
